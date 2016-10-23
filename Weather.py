ddcd939ab78fb42430afd79c54dd74e47b1c2bfb
David Uche <seedsofvision@gmail.com>
2016-10-23 15:26:54 -0600

    Initial Commit

---
+++ Weather.py
@@ -0,0 +1,38 @@
+from pyowm import OWM
+from twilio.rest import TwilioRestClient
+import json
+
+
+#Constants Baby!!
+TWILIO_ACCOUNT_SID = "YOUR SID"
+TWILIO_AUTH_TOKEN = "YOUR AUTH TOKEN"
+FROM_NUMBER = "YOUR TWILIO NUMBER"
+TO_NUMBER = "YOUR NUMBER"
+PYOWMM_API_KEY = "YOUR PYOWM KEY"
+
+#Assign them
+CLIENT = TwilioRestClient(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN)
+owm = OWM(PYOWM_API_KEY)
+
+
+def getMessage():
+	#Obtain the weather instance
+	weather_data = owm.weather_at_place("Toronto,ca")#You can change the place as per your requirements
+	the_weather = weather_data.get_weather()
+
+	#Query for specific data
+	forecast = str(the_weather.get_detailed_status())
+	#Data stored in jsson in dictionary format. This allows for easy access to individual elements
+	temperature_json = the_weather.get_temperature('celsius')
+	temperature = "%s" % temperature_json["temp"]
+
+	#send message to device through twilio
+	message = CLIENT.messages.create(to = TO_NUMBER, from_ = FROM_NUMBER, body = "There's" +" "+ forecast +" "+ "with a temparature of" 
+		+ " " + temperature +" "+ "degrees celsius"+" "+"today" )
+	return message	
+
+#Run the application
+if __name__ == "__main__":
+
+	getMessage()
+	
